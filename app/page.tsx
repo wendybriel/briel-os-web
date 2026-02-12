f6476f38e40f5f5fa0c2ea046f1c2ff362286466
Wendy Briel <briel.os@outlook.com>
2026-02-12 12:38:37 -0500

    Restore homepage entry point

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "4rem 2rem" }}>
+      <h1>BrielOS</h1>
+      <p>Decision Architecture for Founders</p>
+    </main>
+  );
+}
